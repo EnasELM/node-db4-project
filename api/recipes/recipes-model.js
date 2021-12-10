f4671701c0607d44c58ce87091585135c8ca38fd
Enas <enaselmahdwi@gmail.com>
2021-12-09 21:11:53 -0700

    updata

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,5 @@
+function getRecipeById(recipe_id){
+    return Promise.resolve(`hhhhhhhhhhhhhhh ${recipe_id}`)
+}
+
+module.exports = {getRecipeById}
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,4 +1,13 @@
 const router = require('express').Router()
+Recipe = require('./recipes-model')
+
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+      .then(resource => {
+        res.status(200).json(resource)
+      })
+      .catch(next)
+  })
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(err.status || 500).json({
